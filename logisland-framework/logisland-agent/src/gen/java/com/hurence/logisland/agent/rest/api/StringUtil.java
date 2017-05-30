9c2053de491d72160daf4216c7e0e994fed4d052
Stephane Bastian <stephane.bastian@hurence.com>
2017-05-30 15:53:14 +0200

    fixed an issue where the package definition of the class StringUtil does not match the folder the class is defined-in

--- logisland-framework/logisland-agent/src/gen/java/com/hurence/logisland/agent/rest/api/StringUtil.java
+++ logisland-framework/logisland-agent/src/gen/java/com/hurence/logisland/agent/rest/api/StringUtil.java
@@ -1,4 +1,4 @@
-package com.hurence.logisland.agent.rest;
+package com.hurence.logisland.agent.rest.api;
 
 @javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2017-03-23T11:55:20.570+01:00")
 public class StringUtil {
